3cd5d071e0633437c33e6f3dc85c576719b92fa4
antonbiluta <antonbiluta@gmail.com>
2024-02-10 11:31:40 +0300

    #5: Change routes

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,11 +1,7 @@
 export const fetchMemes = async (chatPrefix) => {
-    const protocol = window.location.protocol
-    const host = window.location.hostname;
-    const port = process.env.REACT_APP_BACKEND_PORT;
     const api_path = process.env.REACT_APP_BACKEND_API_PATH;
-    const BACKEND_API_URL = `${protocol}//${host}:${port}${api_path}`;
     try {
-        const response = await fetch(`${BACKEND_API_URL}/memes/${chatPrefix}/last?limit=5`);
+        const response = await fetch(`${api_path}/memes/${chatPrefix}/last?limit=5`);
         if (!response.ok) {
             throw new Error('Failed to fetch memes');
         }

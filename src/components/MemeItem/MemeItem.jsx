c5e124102a4abd45cafdf99fb4a70ce11b69b51a
antonbiluta <antonbiluta@gmail.com>
2024-02-10 11:44:19 +0300

    #5: Change class to className

--- src/components/MemeItem/MemeItem.jsx
+++ src/components/MemeItem/MemeItem.jsx
@@ -4,8 +4,8 @@ import './MemeItem.css'
 const MemeItem = ({ meme }) => {
     return (
         <div className="meme-item" key={meme.id}>
-            <p class="author">{`Мем прислал `}<a href={`https://t.me/${meme.author}`} target="_blank" rel="noopener noreferrer">{`@${meme.author}`}</a></p>
-            <div class="media-content">
+            <p className="author">{`Мем прислал `}<a href={`https://t.me/${meme.author}`} target="_blank" rel="noopener noreferrer">{`@${meme.author}`}</a></p>
+            <div className="media-content">
                 {meme.mediaType === 'image' && <img src={`data:image/png;base64,${meme.media}`} alt="Meme" />}
                 {meme.mediaType === 'video' && <video controls><source src={`data:video/mp4;base64,${meme.media}`} type="video/mp4" /></video>}
             </div>

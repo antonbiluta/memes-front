18a2b6a09533d467c4b3bdbbcf2fd7a1cae07d82
antonbiluta <antonbiluta@gmail.com>
2024-02-10 10:53:13 +0300

    #4: Change env

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,11 @@
+const host = location.hostname;
+const port = process.env.REACT_APP_BACKEND_PORT
+const api_path = process.env.REACT_APP_BACKEND_API_PATH
+const BACKEND_API_PATH = `${host}:${port}${api_path}`;
+
 export const fetchMemes = async (chatPrefix) => {
     try {
-        const response = await fetch(`${process.env.REACT_APP_BACKEND_API_URL}/memes/${chatPrefix}/last?limit=5`);
+        const response = await fetch(`${BACKEND_API_PATH}/memes/${chatPrefix}/last?limit=5`);
         if (!response.ok) {
             throw new Error('Failed to fetch memes');
         }

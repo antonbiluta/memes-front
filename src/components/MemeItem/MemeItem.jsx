c80e622b6ac7705802df877a75c2055174573563
antonbiluta <antonbiluta@gmail.com>
2024-02-24 18:26:47 +0300

    #7: Change video component

--- src/components/MemeItem/MemeItem.jsx
+++ src/components/MemeItem/MemeItem.jsx
@@ -6,8 +6,18 @@ const MemeItem = ({ meme }) => {
         <div className="meme-item" key={meme.id}>
             <p className="author">{`Мем прислал `}<a href={`https://t.me/${meme.username}`} target="_blank" rel="noopener noreferrer">{`@${meme.username}`}</a></p>
             <div className="media-content">
-                {meme.fileType.toLowerCase() === 'image' && <img src={`https://minio-back.biluta.ru/${meme.filePath}`} alt="Meme" />}
-                {meme.fileType.toLowerCase() === 'video' && <video controls><source src={`https://minio-back.biluta.ru${meme.filePath}`} type="video/mp4" /></video>}
+
+            { meme.fileType.toLowerCase() === 'image' ? (
+                <img src={`https://minio-back.biluta.ru/${meme.filePath}`} alt="Meme" />
+            ) : (
+                <video autoPlay muted loop>
+                    <source src={`https://minio-back.biluta.ru/${meme.filePath}`} type="video/mp4" />
+                </video>
+            )}
+
+
+                {/* {meme.fileType.toLowerCase() === 'image' && <img src={`https://minio-back.biluta.ru/${meme.filePath}`} alt="Meme" />} */}
+                {/* {meme.fileType.toLowerCase() === 'video' && <video autoplay muted loop><source src={`https://minio-back.biluta.ru/${meme.filePath}`} type="video/mp4" /></video>} */}
             </div>
         </div>
     );
